fffe927274e0f36d876a3ac9dbd4a76e57c65779
Dhanvi Chaudhary <baddiesforeva05@gmail.com>
2026-09-14 16:49:33 +0530

    Time: 1 ms (99.18%), Space: 42.8 MB (8.79%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int mySqrt(int x) {
+
+        if(x<2){
+            return x;
+        }
+        int left=1;
+        int right=x;
+        int ans=0;
+
+        while(left<=right){
+           int mid=left+(right-left)/2;
+
+            //mid*mid<=n is the same as mid<=n/mid
+            if(mid<=x/mid){
+                ans=mid;
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return ans;
+    }
+}
